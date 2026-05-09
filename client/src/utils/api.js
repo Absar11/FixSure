5c80b1176040fe3a1e97860f4fb650f05a282d3b
Absar11 <siddiqui.mr10@gmail.com>
2026-05-09 17:47:28 +0530

    debug api url

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const API_BASE_URL = import.meta.env.VITE_API_URL || `http://${window.location.hostname}:5000`;
+console.log('FixSure API connected to:', API_BASE_URL);
 
 const api = axios.create({
   baseURL: API_BASE_URL
